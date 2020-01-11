0f26922bebf51d7611d2917922890857ca13f988
Jack Parker <jprkr96@gmail.com>
2020-01-11 17:43:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def base_hash
-	{:railroads => "rainbow" }
+	{:railroads => {} }
 end
 
 def monopoly_with_second_tier
- {:railroads => "rainbow" }
+ {:railroads => {} }
 end
 
 def monopoly_with_third_tier

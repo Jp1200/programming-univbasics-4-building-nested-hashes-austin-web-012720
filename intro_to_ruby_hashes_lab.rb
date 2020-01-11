7c5a64ce37459f3e9cc2458f3f88081cc1450bb9
Jack Parker <jprkr96@gmail.com>
2020-01-11 18:02:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ end
 def monopoly_with_third_tier
  {:railroads => {:pieces => 4 ,
  :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 75, :four_pieces_owned => 100},
- :names => {:reading_railroad =>{},:pennsylvania_railroad => {},}}
+ :names => {:reading_railroad =>{},:pennsylvania_railroad => {},:b_and_o_railroad => {},:shortline_railroad => {}}}
   }
 end
 
